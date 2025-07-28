e7deaf1060a28e30f75157f6f459bf033f2e704e
anvinps <anvinssb@gmail.com>
2025-07-28 18:35:18 +0530

    htmlfor counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment } from './store/counterSlice'
 
 const App = () => {
+  const count=useSelector((state)=>state.counter.value)
+  const dispatch=useDispatch()
   return (
-    <div>App</div>
+    <>
+    <h1 style={{textAlign:"center"}}>{count}</h1>
+   <div style={{display:"flex",justifyContent:"center",gap:"10px"}}> 
+    <button onClick={()=>dispatch(decrement())}>-</button>
+    <button onClick={()=>dispatch(increment())}>+</button>
+    </div>
+    </>
   )
 }
 
