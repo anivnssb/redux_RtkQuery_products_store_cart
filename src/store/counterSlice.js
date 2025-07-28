4caeef3a8a8da7fc221511ee1503e156ba7c94c9
anvinps <anvinssb@gmail.com>
2025-07-28 18:35:54 +0530

    store and couter slice error fixes

--- src/store/counterSlice.js
+++ src/store/counterSlice.js
@@ -6,12 +6,12 @@ const counterSlice=createSlice(
     initialState:{value:0},
     reducers:{
         increment(state){
-            state.value+1;
+            state.value+=1;
         },
         decrement(state){
-            state.value-1;
+            state.value-=1;
         }
 }
 })
-export const {increment,decrement,incrementByAmount}=counterSlice.actions;
+export const {increment,decrement}=counterSlice.actions;
 export default counterSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterSlice from "./counterSlice";
+import counterStore from "./counterSlice";
 
 const store=configureStore({
     reducer:{
-        counter:counterSlice
+        counter:counterStore
 
     }
 })
