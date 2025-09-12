6180d99a853e206c5fdbc2fbc4f29df207ee8111
anivnssb <anvinssb@gmail.com>
2025-09-12 10:05:48 +0530

    router installed, navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,19 @@ import AllProducts from "./components/AllProducts";
 import type { RootState } from "./store/store";
 import type { AppDispatch } from "./store/store";
 import "./app.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 const App: React.FC = () => {
   const count = useSelector((state: RootState) => state.counter.value);
   const dispatch = useDispatch<AppDispatch>();
   return (
-    <>
-      <div>
-        <AllProducts />
-      </div>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/home" element={<AllProducts />} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>Home</li>
+        <li>Cart</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
